2355738c21abc2489cdf9366908cd8510c3d9995
jeremychoisy <choisy.jeremy@gmail.com>
2018-09-27 10:11:38 +0200

    UpdateSys

---
+++ Sys & Res/TP2/fork0.c
@@ -0,0 +1,30 @@
+/* -----------------------------------------------------------------
+* file : fork0.c
+* This program illustrates the use of fork() and
+* getpid() system calls.
+
+* Note that write() is used instead of * printf() since the latter
+* is buffered while the former is not.
+*
+----------------------------------------------------------------- */
+#include  <stdio.h>
+#include  <string.h>
+#include <unistd.h>
+#include  <sys/types.h>
+
+#define   MAX_COUNT  100
+#define   BUF_SIZE   50
+
+int main(void){
+  pid_t  pid;
+  int    i;
+  char   buf[BUF_SIZE];
+
+  fork();
+  pid = getpid();
+
+  for (i = 1; i <= MAX_COUNT; i++) {
+    sprintf(buf, "This line is from pid %d, value = %d\n", pid, i);
+    write(1, buf, strlen(buf)); /* write pour eviter la bufferisation */
+  }
+}
