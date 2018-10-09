08c04120d7e4ac3823ceab92959ccb993adcffef
jeremychoisy <choisy.jeremy@gmail.com>
2018-10-09 15:42:44 +0200

    Sys

--- Sys & Res/TP3/shell.c
+++ Sys & Res/TP3/shell.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "function.h"
+#include <signal.h>
+#include <string.h>
 
 
 int main ( int argc , char *argv [ ] ) {
-
 // Init : Load config files, if any
 // Interp : Run Command loop
   sh_loop ( ) ;
