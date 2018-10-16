35ef6e26cb207fe0ddb5699ce378b70f844dae98
Jérémy Choisy <choisy.jeremy@gmail.com>
2018-10-16 22:39:42 +0200

    Sys

--- Sys & Res/TP3/function.c
+++ Sys & Res/TP3/function.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -48,108 +49,37 @@ char ** sh_split_line ( char * line ) {
 #define BUFFSIZE 100
 
 int sh_execute ( char ** args, char ** forbiddens ) {
-  int i=1,j=0,execR=0;
-  char * strbuff = malloc(sizeof(char)*BUFFSIZE);
-  char * strEnv = malloc(sizeof(char)*BUFFSIZE);
-// cas du newf
-    if(strcmp(args[0],"newf")==0)
+  int i=0, j=0, execR=0;
+  // On compare la liste de tokens de forbiddens avec celle d'args.
+  while(args[i]!=NULL)
+  {
+    j=0;
+    while(forbiddens[j]!=NULL)
     {
-      // On vérifie que l'argument n'est pas déjà présent dans la liste des forbiddens.
-      while(args[i]!=NULL)
+      // Si on trouve une correspondance, il s'agit d'une commande ou d'un arg
+      // censuré, on interrompt donc la fonction sh_execute.
+      if(strcasestr(args[i],forbiddens[j])!=NULL)
       {
-        while(forbiddens[j]!=NULL)
-        {
-          if(strcmp(args[i],forbiddens[j])==0)
-          {
-            printf("Contrainte déjà présente dans la variable FORBIDDEN !\n");
-            exit(0);
-          }
-          j += 1;
-        }
-        i += 1;
-        j=0;
+        printf("Travaille au lieu de jouer !\n");
+        return EXIT_SUCCESS;
       }
-      i=1,j=0;
-      /* On construit la liste des forbiddens sous la forme d'une chaîne de caractères
-        adéquate. */
-      while(forbiddens[j]!=NULL)
-      {
-        strbuff=strcat(strbuff,forbiddens[j]);
-        strbuff=strcat(strbuff,",");
-        j += 1;
-      }
-      // On concatène celle-ci avec les nouveaux verrous passés en arguments.
-      while(args[i]!=NULL)
-      {
-        strbuff=strcat(strbuff,args[i]);
-        strbuff=strcat(strbuff,",");
-        i += 1;
-      }
-      // On retire le dernier caractère ",".
-      strEnv = strncat(strEnv, strbuff, strlen(strbuff)-1);
-      setenv("FORBIDDEN",strEnv,1);
+      j += 1;
     }
-// cas du rmf
-    else if(strcmp(args[0],"rmf")==0)
-    {
-// On construit une chaîne de caractère des forbiddens qu'on place dans strbuff.
-        while(forbiddens[j]!=NULL)
-        {
-          strbuff = strcat(strbuff,forbiddens[j]);
-          strbuff = strcat(strbuff,",");
-          j += 1;
-        }
-// On compare chaque argument à tous les éléments forbiddens.
-        while(args[i]!=NULL)
-        {
-          /* Si on trouve l'argument dans la chaîne de caractère construit
-          au préalable, on crée une nouvelle chaîne avec les éléments précédents
-          et suivants celui-ci. */
-          if(strstr(strbuff,args[i])!=NULL)
-          {
-            strEnv = strncat(strEnv, strbuff, strstr(strbuff,args[i])-strbuff);
-            strEnv = strncat(strEnv,strstr(strbuff,args[i])+strlen(args[i]) + 1, strlen(strbuff));
-            /* On met la valeur de strEnv dans strbuff et on réinitialise strEnv
-               pour pouvoir travailler de manière récursive dans la cas de plusieurs
-               arguments. */
-            strcpy(strbuff,strEnv);
-            strcpy(strEnv,"");
-          }
-          i += 1;
-        }
-        // On enlève la dernière virgule avant de set FORBIDDEN
-        strEnv = strncat(strEnv, strbuff, strlen(strbuff)-1);
-        setenv("FORBIDDEN",strEnv,1);
-    }
-// cas général
-    else
-    {
-      i=0,j=0;
-      while(args[i]!=NULL)
-      {
-        while(forbiddens[j]!=NULL)
-        {
-          if(strstr(args[i],forbiddens[j])!=NULL)
-          {
-            printf("Travaille au lieu de jouer !\n");
-            exit(0);
-          }
-          j += 1;
-        }
-        i += 1;
-        j= 0;
-      }
-      execR=execvp(args[0],args);
-      if(execR!=0)
-      {
-        printf("l3miageshell: %s : commande introuvable.\n",args[0]);
-      }
+    i += 1;
+  }
+  // Sinon on execute la commande
+  execR=execvp(args[0],args);
+  // execvp n'aura un retour que si et seulement son execution s'est mal
+  // déroulé.
+  if(execR!=0)
+  {
+    printf("l3miageshell: %s : commande introuvable.\n",args[0]);
   }
 }
 
 /*======================================================*/
+// Handler du signal SIGCHLD */
 void handlerSIGCHLD(int sig){
-/* Handler du signal SIGCHLD */
   int exit_cond ;
   pid_t pid;
   pid =wait(&exit_cond);
@@ -164,63 +94,145 @@ void sh_loop ( void ) {
   char *line ;
   char **args ;
 
+// variables forbidden
   char * f = malloc(sizeof(char)*BUFFSIZE);
   char **forbiddens;
 
+ // variables newf/rmf
+  int i, j, k;
+  int b = 0;
+
+ // variables fork
   int status = 0;
   pid_t pid;
 
+// struc sigaction pour la gestion de SIGCHLD
   struct sigaction action;
 
-
+// Si la variable d'env n'est pas nulle, on l'a récupère et l'on place les différents
+//tokens dans forbiddens. (malloc dans la fonction sh_split_line)
   if(getenv("FORBIDDEN")!=NULL)
   {
     strcpy(f,getenv("FORBIDDEN"));
-    printf("Get FORBIDDEN variable(s) from env : %s\n",f);
+    printf("Récupération des variables FORBIDDEN depuis l'env : %s\n",f);
+    forbiddens=sh_split_line ( f );
   }
+// Sinon on alloue de la mémoire à forbiddens et on place le NULL à la fin.
   else
   {
-    setenv("FORBIDDEN","",0);
-    printf("Initialisation de la variable d'environnement FORBIDDEN.\n ");
+    forbiddens=malloc(BUFFSIZE*sizeof(char*));
+    if(forbiddens==NULL)
+    {
+      fprintf ( stderr , " lsh : allocation   error \n" ) ;
+      exit ( EXIT_FAILURE ) ;
+    }
+    forbiddens[0]=NULL;
+    printf("Initialisation de FORBIDDEN : utilisez newf/rmf pour modifier la liste.\n");
   }
-
   do
   {
-    strcpy(f,getenv("FORBIDDEN"));
-    forbiddens=sh_split_line ( f );
-
     printf ( "%s" , prompt ) ;
     fflush ( stdout ) ;
 
     line = sh_read_line ( ) ;
     args = sh_split_line ( line ) ;
+
     if(args[0]!=NULL)
     {
       // Cas du exit
-      if(strcmp(args[0],"exit")==0)
+      if(strcasecmp(args[0],"exit")==0)
       {
         exit(0);
       }
-      pid=fork();
-      if(pid == -1)
+
+      if(strcasecmp(args[0],"newf")==0)
       {
-        printf("Erreur fork.\n");
+        // On vérifie que l'argument n'est pas déjà présent dans la liste des forbiddens.
+        // On réinitialise les variables à chaque nouveau passage.
+        b=0;
+        i=1;
+        while(args[i]!=NULL)
+        {
+          j=0;
+          while(forbiddens[j]!=NULL)
+          {
+            if(strcasecmp(args[i],forbiddens[j])==0)
+            {
+              printf("Contrainte déjà présente dans la variable FORBIDDEN !\n");
+              b=1;
+            }
+            j += 1;
+          }
+          i += 1;
+        }
+        if(!b)
+        {
+          // On ajoute les nouveaux tokens à forbiddens, on récupère l'indice de
+          // fin du tableau grâce au j du while précèdent.
+          i=1;
+          while(args[i]!=NULL)
+          {
+              forbiddens[j]=args[i];
+              i++;
+              j++;
+          }
+          forbiddens[j]=NULL;
+        }
       }
-      else if(pid==0)
+      // cas du rmf
+      else if(strcasecmp(args[0],"rmf")==0)
       {
-        status = sh_execute ( args, forbiddens ) ;
+        // On cherche le token ciblé dans forbiddens.
+        i=1;
+        while(args[i]!=NULL)
+        {
+          j=0;
+          while(forbiddens[j]!=NULL)
+          {
+            // Une fois trouvé, on l'écrase en décalant tous les valeurs suivantes
+            // d'une case vers l'arrière et l'on marque la nouvelle fin du tableau
+            // avec NULL.
+            if(strcasecmp(args[i],forbiddens[j])==0)
+            {
+              k=j;
+              while(forbiddens[k+1]!=NULL)
+              {
+                forbiddens[k]=forbiddens[k+1];
+                k++;
+              }
+              forbiddens[k]=NULL;
+            }
+            j += 1;
+          }
+          i += 1;
+        }
+
       }
       else
       {
-        memset(&action,0,sizeof(action));
-        action.sa_handler = handlerSIGCHLD;
-        action.sa_flags=0;
-        sigemptyset(&action.sa_mask);
-        sigaction (SIGCHLD,&action,NULL);
-        sleep(5);
+        pid=fork();
+        if(pid == -1)
+        {
+          printf("Erreur fork.\n");
+        }
+        else if(pid==0)
+        {
+          status = sh_execute ( args, forbiddens ) ;
+          exit(0);
+        }
+        else
+        {
+          // On met en place la gestion de SIGCHLD.
+          memset(&action,0,sizeof(action));
+          action.sa_handler = handlerSIGCHLD;
+          action.sa_flags=0;
+          sigemptyset(&action.sa_mask);
+          sigaction (SIGCHLD,&action,NULL);
+          sleep(5);
+        }
       }
     }
 /* s h_ f r e e ( l i n e ) ; */
 /* s h_ f r e e ( a r g s ) ; */
-} while ( status == 0 ) ;
+  } while ( status == 0 ) ;
 }
--- Sys & Res/TP3/function.h
+++ Sys & Res/TP3/function.h
@@ -3,3 +3,4 @@ int sh_execute ( char ** args, char ** forbiddens ) ;
 char ** sh_split_line ( char * line );
 char * sh_read_line ( void );
 void handlerSIGINT(int sig);
+void handlerSIGCHLD(int sig);
