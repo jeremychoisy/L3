205ab8c2794ccc254980900c6b19d928757836d9
jeremychoisy <choisy.jeremy@gmail.com>
2018-10-22 08:54:37 +0200

    Java/Sys

--- POO/TP3/Vecteur.java
+++ POO/TP3/Vecteur.java
@@ -1,25 +1,61 @@
 class Vecteur{
-  private int n;
-  private int i[];
-
   private double x,y,z,t;
 
   public Vecteur(int ... i){
     switch(i.length){
       case 1:
-        System.out.println("ERREUR");
+        System.out.println("Erreur : arguments manquants.");
         break;
       case 2:
-        this
+        this.x = i[0];
+        this.y = i[1];
+        break;
+      case 3:
+        this.x = i[0];
+        this.y = i[1];
+        this.z = i[2];
+        break;
+      case 4:
+        this.x = i[0];
+        this.y = i[1];
+        this.z = i[2];
+        this.t = i[3];
+      break;
+      default:
+        System.out.println("Erreur : trop d'arguments.");
+      }
     }
-    if(i.length==1){
-      System.out.println("ERREUR");
-    } else if(i.length==2)
-  }
-  public Vecteur(int n, int[] i){
-    this.n = n;
-    this.i = i;
-  }
 
-
-}
+  public Vecteur(int n, int ... i){
+      if(i.length>n){
+        System.out.println("Erreur : trop d'arguments pour la dimension.");
+      }
+      else if(i.length>n){
+        switch(i.length){
+          case 1:
+          this.x = i[0];
+          this.y = 0;
+          this.z = 0;
+          this.t = 0;
+            break;
+          case 2:
+          this.x = i[0];
+          this.y = i[1];
+          this.z = 0;
+          this.t = 0;
+            break;
+          case 3:
+          this.x = i[0];
+          this.y = i[1];
+          this.z = i[2];
+          this.t = 0;
+            break;
+          case 4:
+          this.x = i[0];
+          this.y = i[1];
+          this.z = i[2];
+          this.t = i[3];
+          break;
+          default:
+            System.out.println("Erreur : trop d'arguments.");
+          }
--- Sys & Res/TP3/TP3/function.h
+++
@@ -1,6 +0,0 @@
-void sh_loop ( void ) ;
-int sh_execute ( char ** args, char ** forbiddens ) ;
-char ** sh_split_line ( char * line );
-char * sh_read_line ( void );
-void gestionCtrlC (int sig);
-void gestionFils (int sig);
