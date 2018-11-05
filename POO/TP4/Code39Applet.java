d297742499c400b936aab196bd51eae83c600525
jeremychoisy <choisy.jeremy@gmail.com>
2018-11-05 07:40:18 +0100

    Update

--- POO/TP4/Code39Applet.java
+++ POO/TP4/Code39Applet.java
@@ -3,8 +3,9 @@ import java.applet.Applet;
 
 public class Code39Applet extends Applet {
   public void paint(Graphics g) {
+    Code39 c = new Code39();
     Symbole s;
-    IterateurSymbole i = new IterateurSymbole("010010100");
+    IterateurSymbole i = new IterateurSymbole(c.getCode("JAVA 2012"));
     int x = 5;
 
     while(!i.estTerminee())
